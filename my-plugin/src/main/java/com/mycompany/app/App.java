b3162b0b87e2edfea842d761f87d6702b67d7f5e
Achraf <elamraniachraf93@gmail.com>
2016-02-15 14:29:40 +0100

    récupération de info sur le projet destination

--- my-plugin/src/main/java/com/mycompany/app/App.java
+++ my-plugin/src/main/java/com/mycompany/app/App.java
@@ -7,18 +7,18 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
-import java.io.File;
-
 
 @Mojo( name = "sayhi")
 public class App extends AbstractMojo {
-
-
+    /**
+     * permet la récupération d'information sur le project qui utilise notre plugin
+     * la variable project est remplie autmatiquement
+     */
     @Parameter(defaultValue = "${project}", required = true, readonly = true)
     private MavenProject project;
     public void execute() throws MojoExecutionException, MojoFailureException {
         System.out.println("yeaaaaaaaaaaaaaaaaaaaaaaaaaah\nligne2\nligne3\n");
-        System.out.println("source :  "+project.toString());
+        System.out.println("source :  "+project.getBasedir());
 
     }
 }
