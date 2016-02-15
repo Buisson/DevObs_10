f2e4652dc3f47d07a4a7784de8c31ed01fea0b4f
Achraf <elamraniachraf93@gmail.com>
2016-02-15 14:11:04 +0100

    récuperer des infos sur le project destination

--- my-plugin/src/main/java/com/mycompany/app/App.java
+++ my-plugin/src/main/java/com/mycompany/app/App.java
@@ -4,17 +4,21 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+import java.io.File;
 
 
-/**
- * Hello world!
- *
- */
 @Mojo( name = "sayhi")
-public class App extends AbstractMojo
-{
+public class App extends AbstractMojo {
+
+
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
+    private MavenProject project;
     public void execute() throws MojoExecutionException, MojoFailureException {
         System.out.println("yeaaaaaaaaaaaaaaaaaaaaaaaaaah\nligne2\nligne3\n");
+        System.out.println("source :  "+project.toString());
 
     }
 }
